b990eb01cb42b7d8466cbc6542f66ae23641dcd6
Casey Heagerty <cheagerty@gmail.com>
2020-03-26 14:52:33 +0100

    MatrixTraining

--- src/exercices/MatrixTraining.java
+++ src/exercices/MatrixTraining.java
@@ -8,7 +8,7 @@ public class MatrixTraining {
      */
     public int[][] emptyIntMatrix(int lines, int columns) {
 
-        return null;
+        return emptyIntMatrix(lines, columns);
     }
 
     /**
@@ -19,7 +19,7 @@ public class MatrixTraining {
      */
     public String[][] emptyStringMatrix(int lines, int columns) {
 
-        return null;
+        return emptyStringMatrix(lines, columns);
     }
 
     /**
@@ -30,7 +30,7 @@ public class MatrixTraining {
      */
     public int[][] intMatrix(int[] a, int[] b, int[] c) {
 
-        return null;
+        return intMatrix(a, b, c);
     }
 
     /**
@@ -41,7 +41,7 @@ public class MatrixTraining {
      */
     public String[][] stringMatrix(String[] a, String[] b, String[] c) {
 
-        return null;
+        return stringMatrix(a, b, c);
     }
 
     /**
@@ -50,7 +50,7 @@ public class MatrixTraining {
      */
     public int height(int[][] matrix) {
 
-        return 0;
+        return height(matrix);
     }
 
     /**
@@ -59,7 +59,7 @@ public class MatrixTraining {
      */
     public int width(int[][] matrix) {
 
-        return 0;
+        return width(matrix);
     }
 
     /**
@@ -70,7 +70,7 @@ public class MatrixTraining {
      */
     public int valueAtPosition(int[][] matrix, int line, int column) {
 
-        return 0;
+        return matrix[line][column];
     }
 
     /**
@@ -83,7 +83,8 @@ public class MatrixTraining {
      */
     public int[][] replace(int[][] matrix, int value, int line, int column) {
 
-        return null;
+        matrix[line][column] = value;
+        return matrix;
     }
 
     /**
@@ -92,7 +93,7 @@ public class MatrixTraining {
      */
     public int sum(int[][] matrix) {
 
-        return 0;
+        return sum(matrix);
     }
 
     /**
@@ -102,7 +103,7 @@ public class MatrixTraining {
      */
     public boolean contains(int[][] matrix, int search) {
 
-        return false;
+        return contains(matrix, search);
     }
 
     /**
@@ -111,7 +112,7 @@ public class MatrixTraining {
      */
     public int countEvens(int[][] matrix) {
 
-        return 0;
+        return countEvens(matrix);
     }
 
     /**
@@ -121,6 +122,6 @@ public class MatrixTraining {
      */
     public int occurrences(char[][] matrix, char search) {
 
-        return 0;
+        return occurrences(matrix, search);
     }
 }
